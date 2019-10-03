237f43851bbfa8e36b0948612789f75eda7a0616
orumsventus <orumsventus@gmail.com>
2019-10-03 13:28:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Muebleria_app" 
---
+++ app/src/main/java/com/example/muebleria_app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.muebleria_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+    }
+}
