15b20d8934f3dc9aef96244ddfa6c3345eee8ad0
orumsventus <orumsventus@gmail.com>
2019-10-08 12:02:11 -0500

    Editado Correcto

--- app/src/main/java/com/example/muebleria_app/fragments/MuebleDetalleFragment.java
+++ app/src/main/java/com/example/muebleria_app/fragments/MuebleDetalleFragment.java
@@ -19,8 +19,10 @@ import android.widget.Toast;
 import com.example.muebleria_app.Entidades.Mueble;
 import com.example.muebleria_app.R;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -59,9 +61,61 @@ public class MuebleDetalleFragment extends Fragment {
             if ( getArguments().getString("id")!= null ){
                 /////// EDITAR /////////////////////////////////////////////////////////////////////
                 getActivity().setTitle("Editar información");
-                String id = getArguments().getString("id");
+                final String id = getArguments().getString("id");
 
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                Toast.makeText(getContext(),"------ ", Toast.LENGTH_SHORT).show();
+
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference reference = db.collection("muebles").document(id);
+                reference.get().addOnCompleteListener(new OnCompleteListener < DocumentSnapshot > () {
+                    @Override
+                    public void onComplete(@NonNull Task < DocumentSnapshot > task) {
+                        if (task.isSuccessful()) {
+
+                            nombre = (TextView) getView().findViewById(R.id.mueble_detalle_nombre);
+                            descripcion = (TextView) getView().findViewById(R.id.mueble_detalle_descripcion);
+                            precio = (TextView) getView().findViewById(R.id.mueble_detalle_precio);
+                            boton_guardar = (Button) getView().findViewById(R.id.mueble_detalle_boton_guardar);
+
+                            DocumentSnapshot doc = task.getResult();
+                            nombre.setText(doc.get("nombre").toString());
+                            descripcion.setText(doc.get("descripcion").toString());
+                            precio.setText(doc.get("precio").toString());
+
+                            boton_guardar.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(final View v) {
+                                    Map<String, Object> mueble_editado = new HashMap<>();
+                                    mueble_editado.put("nombre", nombre.getText().toString());
+                                    mueble_editado.put("descripcion", descripcion.getText().toString());
+                                    mueble_editado.put("precio", descripcion.getText().toString());
+
+                                    db.collection("muebles").document(id)
+                                            .set(mueble_editado)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Navigation.findNavController(v).navigateUp();
+                                                    Toast.makeText(getContext(),"Mueble editado exitosamente", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Navigation.findNavController(v).navigateUp();
+                                                    Toast.makeText(getContext(),"No se pudo editar", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
 
 
             }
@@ -79,14 +133,14 @@ public class MuebleDetalleFragment extends Fragment {
                 public void onClick(final View v) {
                     Toast.makeText(getContext(),"hols",Toast.LENGTH_SHORT).show();
 
-                    Map<String, Object> city = new HashMap<>();
-                    city.put("nombre", nombre.getText().toString());
-                    city.put("descripcion", descripcion.getText().toString());
-                    city.put("precio", precio.getText().toString());
+                    Map<String, Object> mueble_nuevo = new HashMap<>();
+                    mueble_nuevo.put("nombre", nombre.getText().toString());
+                    mueble_nuevo.put("descripcion", descripcion.getText().toString());
+                    mueble_nuevo.put("precio", precio.getText().toString());
 
                     FirebaseFirestore db = FirebaseFirestore.getInstance();
                     db.collection("muebles")
-                            .add(city)
+                            .add(mueble_nuevo)
                             .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                                 @Override
                                 public void onSuccess(DocumentReference documentReference) {
